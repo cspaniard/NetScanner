ff8f7f6281a8b772dd654e24ed782ed875809a32
David Sanromá <dsanroma@gmail.com>
2024-05-24 19:29:49 +0200

    Lectura optimizada de stdout y stderr.

--- src/Infrastructure/Brokers/Processes/Process.fs
+++ src/Infrastructure/Brokers/Processes/Process.fs
@@ -2,24 +2,25 @@ namespace Brokers.Processes.Process
 
 open System
 open System.Diagnostics
+open System.IO
 open System.Threading
 open Model
 
 type Broker () =
 
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
     static member startProcessTry (processName : string) (arguments : string) =
 
         Process.Start (processName, arguments)
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
 
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
     static member startProcessWithStartInfoTry (startInfo : ProcessStartInfo) =
 
         Process.Start startInfo
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
 
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
     static member startProcessWithTimeOutAsync processName (timeOut : TimeOut) arguments =
 
         let startInfo = ProcessStartInfo (RedirectStandardOutput = true,
@@ -47,9 +48,9 @@ type Broker () =
                 proc.Kill ()
                 return None
         }
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
 
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
     static member startAndWaitForProcessAsyncTry processName arguments =
 
         task {
@@ -58,10 +59,25 @@ type Broker () =
 
             return proc
         }
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
 
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
     static member startProcessAndReadAllLinesAsyncTry processName arguments =
+
+        //--------------------------------------------------------------------------------------------------------------
+        let readAllLinesAsyncTry (reader : StreamReader) =
+
+            let rec readAllLinesAsync (reader : StreamReader) (stdOutLines: ResizeArray<string>) =
+                task {
+                    match! reader.ReadLineAsync () with
+                    | null -> return stdOutLines
+                    | line ->
+                        stdOutLines.Add line
+                        return! readAllLinesAsync reader stdOutLines
+                }
+
+            readAllLinesAsync reader (ResizeArray<string>())
+        //--------------------------------------------------------------------------------------------------------------
 
         backgroundTask {
             let proc =
@@ -74,30 +90,11 @@ type Broker () =
                                   Arguments = arguments)
                 |> Process.Start
 
-            let stdOutLines = ResizeArray<string>()
-            let mutable tmpLine = ""
-
-            // Lectura del StdOut
-            let! line = proc.StandardOutput.ReadLineAsync ()
-            tmpLine <- line
-
-            while tmpLine <> null do
-                stdOutLines.Add tmpLine
-                let! line = proc.StandardOutput.ReadLineAsync ()
-                tmpLine <- line
-
-            // Lectura del StdErr
-            let stdErrLines = ResizeArray<string>()
-            let! line = proc.StandardError.ReadLineAsync ()
-            tmpLine <- line
-
-            while tmpLine <> null do
-                stdErrLines.Add tmpLine
-                let! line = proc.StandardError.ReadLineAsync ()
-                tmpLine <- line
+            let! stdOutLines = readAllLinesAsyncTry proc.StandardOutput
+            let! stdErrLines = readAllLinesAsyncTry proc.StandardError
 
             do! proc.WaitForExitAsync ()
 
             return (stdOutLines.ToArray (), stdErrLines.ToArray (), proc.ExitCode)
         }
-    //----------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
