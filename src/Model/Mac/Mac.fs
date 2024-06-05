3e9870c1a2eb865c2738db27be00304a2dc66873
David Sanromá <dsanroma@gmail.com>
2024-06-05 16:37:42 +0200

    Mac: Mejor validación y muestra la Mac en el error.

--- src/Model/Mac/Mac.fs
+++ src/Model/Mac/Mac.fs
@@ -7,32 +7,23 @@ open Model.MacValidation
 type Mac =
     private Mac of string
 
-        static member clean (value : string) =
-            let validChars = Array.append [| '0'..'9' |] [| 'A'..'F' |]
-
-            value
-                .ToUpper()
+        static member private cleanValue (value : string) =
+            (value
+                |> trim
+                |> toUpper)
                 .ToCharArray ()
-            |> Array.fold (fun st c -> if validChars |> Array.contains c
+            |> Array.fold (fun st c -> if Char.IsLetterOrDigit c
                                        then st + c.ToString ()
                                        else st) ""
-
-        static member private canonicalize (value : string) =
-
-            checkNull value
-
-            value
-            |> trim
-            |> toUpper
 
         static member private validateTry (value : string) =
 
             try
                 getValidatorsList ()
-                |> Array.iter (fun f -> f value)
+                |> Array.iter (fun f -> f (Mac.cleanValue value))
 
                 value
-            with e -> failwith $"Mac: {e.Message}"
+            with e -> failwith $"Mac ({value}): {e.Message}"
 
         member this.value = let (Mac value) = this in value
         member this.hasValue = let (Mac value) = this in value |> (not << String.IsNullOrWhiteSpace)
@@ -54,7 +45,5 @@ type Mac =
 
         static member create (value : string) =
             value
-            |> Mac.clean
-            |> Mac.canonicalize
             |> Mac.validateTry
             |> Mac
--- src/Model/Mac/MacValidation.fs
+++ src/Model/Mac/MacValidation.fs
@@ -6,13 +6,13 @@ let checkNull (macString : string) =
     macString = null |> failWithIfTrue "El dato proporcionado no puede ser null."
 
 let checkEvenLength (macString : string) =
-    macString.Length % 2 = 0 |> failWithIfFalse "La longitud tiene que se mútiplo de 2."
+    macString.Length % 2 = 0 |> failWithIfFalse "La longitud tiene que ser mútiplo de 2."
 
 let checkValidChars (macString : string) =
     let validChars = Array.append [| '0'..'9' |] [| 'A'..'F' |]
 
     macString.ToCharArray ()
-    |> Array.iter (fun c -> validChars |> Array.contains c |> failWithIfFalse "Caracteres inválidos.")
+    |> Array.iter (fun c -> validChars |> Array.contains c |> failWithIfFalse "Contiene caracteres inválidos.")
 
 
 let getValidatorsList () =
